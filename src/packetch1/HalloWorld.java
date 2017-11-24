6bb9a15941cf492028277a1c6098a71806000720
Matthias Dönni <matthiasd19@gmail.com>
2017-11-24 09:04:17 +0100

    Upload1

---
+++ src/packetch1/HalloWorld.java
@@ -0,0 +1,13 @@
+package packetch1;
+
+public class HalloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hallo Wolrd");
+		
+		
+	}
+
+}
